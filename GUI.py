8755d0614d2e960227e0dd1598aee0dfef7b82cd
clamfighter1 <48677268+clamfighter1@users.noreply.github.com>
2021-07-27 18:41:06 +0100

    GUI added

---
+++ GUI.py
@@ -0,0 +1,87 @@
+from os import name
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter.font import Font
+import tkinter.font as font
+import WebScraping
+import csv
+import os
+
+
+#Roboto
+
+master = Tk()
+master.geometry("500x200")
+master.title('Company Data Bot')
+master['bg'] = '#fcfaea'
+
+
+text = tk.Text(master)
+myFont = font.Font(family='Roboto')
+text.configure(font=myFont)
+
+master.columnconfigure(0, weight=1)
+master.columnconfigure(1, weight=2)
+
+
+
+Label(master, text='Company Name File',bg='#fcfaea',fg='#262626',font=myFont).grid(row=0)
+Label(master, text='',bg='#fcfaea',fg='#262626',font=myFont).grid(row=1,column=4)
+Label(master, text='Criteria Text',bg='#fcfaea',fg='#262626',font=myFont).grid(row=2)
+Label(master, text='',bg='#fcfaea',fg='#262626',font=myFont).grid(row=3)
+Label(master, text='Download Folder',bg='#fcfaea',fg='#262626',font=myFont).grid(row=4)
+
+
+folder_path = StringVar()
+
+
+def browse_button_companyNameDir():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path
+    filename = filedialog.askopenfile()
+    print(filename.name)
+    folder_path.set(filename)
+    e_companyNameDir.insert(0,filename.name)
+
+def browse_button_DownloadDir():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path
+    filename = filedialog.askdirectory()
+    filename = os.path.normpath(filename) 
+    print(filename)
+    folder_path.set(filename)
+    e_DownloadDir.insert(0,filename)
+
+def onPress():
+    print('Button Pressed')
+    namePath = e_companyNameDir.get()
+    namePath.encode('unicode_escape')
+    print(namePath)
+    with open(namePath,'r') as csv_file:
+        copmanies = csv_file.read().splitlines() 
+    print(copmanies)
+    WebScraping.WebScraper(
+        copmanies,
+        e_criteria.get(),
+        e_DownloadDir.get()
+    )
+
+
+e_companyNameDir = Entry(master,font=myFont)
+e_criteria = Entry(master,font=myFont)
+e_DownloadDir = Entry(master,font=myFont)
+e_companyNameDir.grid(row=0, column=1)
+e_criteria.grid(row=2, column=1)
+e_DownloadDir.grid(row=4, column=1)
+
+browseButtonNames = tk.Button(master, text='Select File', command=browse_button_companyNameDir,bg='#faf4ef',fg='#262626',font=myFont)
+browseButtonDownload = tk.Button(master, text='Select Folder', command=browse_button_DownloadDir,bg='#faf4ef',fg='#262626',font=myFont)
+submitButton = tk.Button(master, text='Start Bot', command=onPress,bg='#faf4ef',fg='#262626',font=myFont)
+submitButton.grid(row=5, column=1)
+browseButtonNames.grid(row=0,column=3)
+browseButtonDownload.grid(row=4, column=3)
+mainloop()
--- WebScraping.py
+++ WebScraping.py
@@ -1,8 +1,10 @@
 import getpass
 import argparse, os, time
 import random
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 from bs4 import SoupStrainer
@@ -11,63 +13,98 @@ import os
 import pandas as pd
 import re
 import numpy as np
+import time
 
-CompanyNames = ['NEXT','greene king']
-link = ('https://www.google.com/')
-criteria = 'co2e'
 
 '''
 Runs for each name in the company names list
 Creates a new browser object each time so the download location can be changed
 this allows each company pdfs to be downloaded in the same folder.
 '''
-for name in CompanyNames:
-    #create folder for PDFs to go in
-    filepath = r'F:\Downloads'+'\\'+name
-    try:
-        os.mkdir(filepath)
-    except FileExistsError:
-        print('FOLDER ALREADY EXISTS: '+filepath)
-    print(filepath)
-    #Defining the webdriver. Set options so it can download PDFs
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('prefs', {
-    "download.default_directory": filepath, #Change default directory for downloads
-    "download.prompt_for_download": False, #To auto download the file
-    "download.directory_upgrade": True,
-    "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
-    })
-    browser = webdriver.Chrome(r"C:\webdrivers\chromedriver.exe",options=options)
-    time.sleep(random.uniform(3, 4.9))
 
-    #Load Google
-    browser.get(link)
-    time.sleep(random.uniform(3, 4.9))
+def WebScraper(companyNames,Criteria,downloadPath):
+    CompanyNames = companyNames
+    criteria = Criteria
+    downloadPath = downloadPath
+    link = ('https://www.google.com/')
+    for name in CompanyNames:
+        #create folder for PDFs to go in
+        filepath = downloadPath+'\\'+name
+        try:
+            os.mkdir(filepath)
+        except FileExistsError:
+            print('FOLDER ALREADY EXISTS: '+filepath)
+        print(filepath)
+        #Defining the webdriver. Set options so it can download PDFs
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {
+        "download.default_directory": filepath, #Change default directory for downloads
+        "download.prompt_for_download": False, #To auto download the file
+        "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
+        })
+        options.add_argument("start-maximized");
+        options.add_argument("disable-infobars")
+        options.add_argument("--disable-extensions")
+        browser = webdriver.Chrome(r"C:\webdrivers\chromedriver.exe",options=options)
+        time.sleep(random.uniform(3, 4.9))
 
-    #Search for given Criteria
-    search = browser.find_element_by_name('q')
-    search.send_keys(name+" "+criteria+" filetype:pdf")
-    search.send_keys(Keys.RETURN) # hit return after you enter search text
-    time.sleep(5) # sleep for 5 seconds so you can see the results
+        #Load Google
+        browser.get(link)
+        time.sleep(random.uniform(3, 4.9))
+        button = browser.find_element_by_css_selector('#L2AGLb')
+        ActionChains(browser).click(button).perform()
 
-    #Take html of cite and find all links
-    soup = bs(browser.page_source, features="html.parser")
-    elems = browser.find_elements_by_xpath("//a[@href]")
-    #for each link, if it ends in pdf, download it
-    for elem in elems:
-        if elem.get_attribute("href").endswith(('.pdf')):
-            #print(elem.get_attribute("href"))
-            browser.get(elem.get_attribute("href"))
-    #Wait to make sure each pdf is downloaded before continuing
-    wait = True
-    while wait == True:
-        time.sleep(random.uniform(5, 7.9))
-        wait = False
-        for fname in os.listdir(filepath):
-            if fname.endswith('.crdownload'):
-                wait = True
-        
-    time.sleep(random.uniform(3, 4.9))
-    browser.quit()
 
+        #Search for given Criteria
+        search = browser.find_element_by_name('q')
+        search.send_keys(name+" "+criteria+" filetype:pdf")
+        search.send_keys(Keys.RETURN) # hit return after you enter search text
+        time.sleep(5) # sleep for 5 seconds so you can see the results
+
+        #Take html of cite and find all links
+        soup = bs(browser.page_source, features="html.parser")
+        elems = browser.find_elements_by_xpath("//a[@href]")
+        #for each link, if it ends in pdf, download it
+
+        window_before = browser.window_handles[0] 
+
+        for elem in elems:
+            try:
+                if elem.get_attribute("href").endswith(('.pdf')):#If element is  pdf link
+                    pdflink = elem.get_attribute("href")#get link
+                    browser.execute_script("window.open('{}');".format(''))#open blank tab
+                    window_after = browser.window_handles[1]
+                    browser.switch_to.window(window_after)#swap to new tab
+                    browser.get(pdflink)#search pdf link (if its a pdf it will autodownload)
+                    browser.close()#close tab
+                    browser.switch_to.window(window_before)#swap back to original tab
+            except selenium.common.exceptions.StaleElementReferenceException:
+                print('StaleElementReferenceException')
+
+        #Wait to make sure each pdf is downloaded before continuing
+        wait = True
+        while wait == True:
+            time.sleep(random.uniform(5, 7.9))
+            wait = False
+            for fname in os.listdir(filepath):
+                if fname.endswith('.crdownload'):
+                    wait = True   
+        time.sleep(random.uniform(3, 4.9))
+        browser.quit()
+
+
+'''
+CompanyNames = ['asda','NEXT','greene king','santander','HSBC','Unilever','aviva','Tesco','airbus','vauxhall']
+criteria = 'co2e'
+downloadPath = r'F:\Webscraping\Downloads'
+
+
+starttime = time.time()
+print(starttime)
+WebScraper(CompanyNames,criteria,downloadPath)
+endtime = time.time()
+print(endtime)
+print(endtime - starttime)
 time.sleep(5)
+'''
