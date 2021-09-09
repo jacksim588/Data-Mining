94601f18814e0c9e28944493dd5442261ea302e3
clamfighter1 <48677268+clamfighter1@users.noreply.github.com>
2021-09-09 23:24:53 +0100

    presentation ready

--- PDF Mining.py
+++ PDF Mining.py
@@ -1,36 +1,61 @@
+from WebScraping import CompanyNames
 import PyPDF2
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import os
 
-CompanyNames = ['asda','NEXT','greene king','santander','HSBC','Unilever','aviva','Tesco','airbus','vauxhall']
+filepath_down = r'F:\WebScraping\Downloads'
+CompanyDirs = []
+
+directory_contents = os.listdir(filepath_down)
+CompanyNames = [ x for x in directory_contents if ".txt" not in x ]
+print(CompanyNames)
+
 filepath_down = r'F:\Webscraping\Downloads'
 filepath_fil = r'F:\Webscraping\Filtered'
+errorFiles = []
+errors = []
 for name in CompanyNames:
     currfilepath = filepath_down+'\\'+name
-
+    print('Beggining '+name+' files')
     try:
         os.mkdir(filepath_fil+'\\'+name)
+        #print('Created Directory')
     except FileExistsError:
         print('FOLDER ALREADY EXISTS: '+filepath_fil+'\\'+name)
-
-    for fname in os.listdir(filepath_down+'\\'+name):
-        file = open(filepath_down +'\\'+ name +'\\'+fname,'rb')
-        infile = PdfFileReader(file,'rb')
-        output = PdfFileWriter()
-
+    try:
+        for fname in os.listdir(filepath_down+'\\'+name):
+            file = open(filepath_down +'\\'+ name +'\\'+fname,'rb')
+            infile = PdfFileReader(file,'rb')
+            output = PdfFileWriter()
             
-        pages_to_keep = []
-        for i in range(infile.numPages):
-            page = infile.getPage(i)
-            text = page.extractText()
-            if any(ext in text for ext in ['CO2e']):
-                print(i)
-                pages_to_keep.append(i)
-        for i in pages_to_keep:
-            p = infile.getPage(i)
-            output.addPage(p)
-        if not pages_to_keep:
-            print('No Keyword Found: '+name+'\\'+fname)
-        else:
-            with open(filepath_fil +'\\'+name+'\\'+fname, 'wb') as f:
-                output.write(f)
+            pages_to_keep = []
+            
+            try:
+                for i in range(infile.numPages):
+                    page = infile.getPage(i)
+                    text = page.extractText()
+                    if any(ext in text for ext in ['CO2e','scope 1']):
+                        #print(i)
+                        pages_to_keep.append(i)
+                for i in pages_to_keep:
+                    p = infile.getPage(i)
+                    output.addPage(p)
+            
+                if not pages_to_keep:
+                    print('No Keyword Found: '+name+'\\'+fname)
+                else:
+                    with open(filepath_fil +'\\'+name+'\\'+fname, 'wb') as f:
+                        output.write(f)
+            except (PyPDF2.utils.PdfReadError,KeyError,PermissionError,ValueError) as e:
+                print('Error: ',fname)
+                errorFiles.append(fname)
+                errors.append(e)
+    except (PermissionError,PyPDF2.utils.PdfReadError) as e:
+        print('Error: ',fname)
+        errorFiles.append(fname)
+        errors.append(e)
+
+with open(filepath_fil+'\Errors.txt', 'w') as f:
+    for item in errorFiles:
+        f.write("%s\n" % item)
+print(errors)
